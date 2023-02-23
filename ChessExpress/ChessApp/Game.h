1af353d7593b5d8fe7318444a9493f315ddb918c
Lethiwe <lethiwe.mwendwa@gmail.com>
2023-02-23 23:34:46 +0000

    Finished the board display. But window is not resizable, also the board tiles are not really linked to anything else.... like the array... I should get to fixing that tbh. Anyway, PROGRESS :D

---
+++ ChessExpress/ChessApp/Game.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "SDL.h"
+
+class Game
+{
+public:
+	Game();
+	~Game();
+
+
+private:
+
+
+};
+
--- ChessExpress/ChessApp/main.cpp
+++ ChessExpress/ChessApp/main.cpp
@@ -1,25 +1,97 @@
 #include "SDL.h"
 using namespace std;
 
+const int boardSize = 8;
+const int tileSize = 70;
+int SCREEN_WIDTH = 560;
+int SCREEN_HEIGHT = 560;
+
+void drawBoard(SDL_Renderer* renderer, int boardSize, int tileSize) {
+	int tileHeightInc = 0;
+
+	for (int i = 0; i < boardSize; i++) {
+		int tileWidthInc = 0;
+
+		for (int j = 0; j < boardSize; j++) {
+			//i is the row, j is the column
+			//draw square
+			if (((i + j) % 2) == 0) {
+				SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+			}
+			else {
+				SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+			}
+
+			SDL_Rect tile = { tile.x = tileWidthInc, tile.y = tileHeightInc, tile.h = tileSize, tile.w = tileSize };
+			SDL_RenderFillRect(renderer, &tile);
+			tileWidthInc += tileSize;
+		}
+		tileHeightInc += tileSize;
+	}
+
+	SDL_RenderPresent(renderer);
+};
+
 
 int main(int argc, char* argv[])
 {
-
 	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Window* window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
+
+	/* 
+
+	// My attempt at a FEN notation reader
+	for (int i; i < 8; i++) {
+		for (int j; i < 8; j++) {
+			if (ChessBoard[i][j] == 0):
+				FENnum
+
+		}
+	}
+	*/
+
+	SDL_Window* window = SDL_CreateWindow("Chess Express", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
+	SDL_Event event;
 
-	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
 
-	SDL_RenderClear(renderer);
+	int ChessBoard[boardSize][boardSize] = {
+	{0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0}
+
+	};
+
+	drawBoard(renderer, boardSize, tileSize);
 
 	SDL_RenderPresent(renderer);
 
-	SDL_RenderPresent(renderer);
+	// HERES THE GAME LOOP WOOO
+	bool gameRunning = true;
 
-	SDL_Delay(5000);
+	while (gameRunning) {
 
+		SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
+
+		SDL_RenderClear(renderer);
+
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				gameRunning = false;
+				break;
+			}
+		}
+	}
+
+	SDL_DestroyWindow(window);
+
+	SDL_Quit();
 
 	return 0;
 }
