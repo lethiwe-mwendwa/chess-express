293684ad745cb2fcf203c2a32561d42153679ad8
Lethiwe <lethiwe.mwendwa@gmail.com>
2023-02-25 14:16:58 +0000

    WOOOOHOOO Progresss! :D I was able to make the chess board resize each time the window is resized, and makes it constantly centered. Chess board visual is a go. Also figured out why my game loop was so laggy. note to self, dont render stuff in there constantly, only use the game loop for events dingus.

--- ChessExpress/ChessApp/main.cpp
+++ ChessExpress/ChessApp/main.cpp
@@ -2,24 +2,44 @@
 using namespace std;
 
 const int boardSize = 8;
-const int tileSize = 70;
-int SCREEN_WIDTH = 560;
+int SCREEN_WIDTH = 900;
 int SCREEN_HEIGHT = 560;
 
-void drawBoard(SDL_Renderer* renderer, int boardSize, int tileSize) {
-	int tileHeightInc = 0;
+
+
+void drawBoard(SDL_Renderer* renderer, int boardSize, double width, double height) {
+	int tileWidthInc;
+	int tileSize;
+	int tileHeightInc;
+	int widthCord;
+
+	// This part checks the width and height of the screen
+	if (height < width) {
+		tileSize = (height) / 8;
+		tileHeightInc = 0;
+		widthCord = (width - height) / 2;
+	}
+	else {
+		tileSize = (width) / 8; 
+		tileHeightInc = 0 - (width - height) / 2;
+		widthCord = 0;
+
+	}
+
+	// SDL_SetRenderDrawColor(renderer, 5, 50, 70, 255);
+	SDL_SetRenderDrawColor(renderer, 5, 50, 70, 255);
+	SDL_RenderClear(renderer);
 
 	for (int i = 0; i < boardSize; i++) {
-		int tileWidthInc = 0;
-
+		tileWidthInc = widthCord;
 		for (int j = 0; j < boardSize; j++) {
 			//i is the row, j is the column
 			//draw square
 			if (((i + j) % 2) == 0) {
-				SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+				SDL_SetRenderDrawColor(renderer, 232, 226, 198, 255);
 			}
 			else {
-				SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+				SDL_SetRenderDrawColor(renderer, 30, 129, 176, 255);
 			}
 
 			SDL_Rect tile = { tile.x = tileWidthInc, tile.y = tileHeightInc, tile.h = tileSize, tile.w = tileSize };
@@ -27,6 +47,7 @@ void drawBoard(SDL_Renderer* renderer, int boardSize, int tileSize) {
 			tileWidthInc += tileSize;
 		}
 		tileHeightInc += tileSize;
+		
 	}
 
 	SDL_RenderPresent(renderer);
@@ -49,12 +70,13 @@ int main(int argc, char* argv[])
 	}
 	*/
 
-	SDL_Window* window = SDL_CreateWindow("Chess Express", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+	SDL_Window* window = SDL_CreateWindow("Chess Express", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
 
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
 	SDL_Event event;
 
+	/*
 
 	int ChessBoard[boardSize][boardSize] = {
 	{0,0,0,0,0,0,0,0},
@@ -67,26 +89,37 @@ int main(int argc, char* argv[])
 	{0,0,0,0,0,0,0,0}
 
 	};
+	*/
 
-	drawBoard(renderer, boardSize, tileSize);
+	drawBoard(renderer, boardSize, SCREEN_WIDTH,SCREEN_HEIGHT);
 
-	SDL_RenderPresent(renderer);
+
 
 	// HERES THE GAME LOOP WOOO
 	bool gameRunning = true;
 
 	while (gameRunning) {
 
-		SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
 
-		SDL_RenderClear(renderer);
 
 		while (SDL_PollEvent(&event)) {
 			if (event.type == SDL_QUIT) {
 				gameRunning = false;
 				break;
+
+
 			}
+			
+			if (event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) {
+
+				drawBoard(renderer, boardSize, event.window.data1, event.window.data2);
+				 
+			}
+			
+			SDL_Delay(10);
 		}
+
+
 	}
 
 	SDL_DestroyWindow(window);
