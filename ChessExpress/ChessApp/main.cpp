a7d5df8e5ea2371bd066b0e0735bc7758e8eceb3
Lethiwe <lethiwe.mwendwa@gmail.com>
2023-02-28 03:59:50 +0000

    Basically dealt with a dataleak (didnt destory texture and surface. Note to self, DO THAT MORE). Displayed chess peices in a really bad way just to test functionality. I will now make classes as well as make a FEN notation reader

--- ChessExpress/ChessApp/main.cpp
+++ ChessExpress/ChessApp/main.cpp
@@ -4,9 +4,11 @@
 using namespace std;
 
 const int boardSize = 8;
-int SCREEN_WIDTH = 900;
-int SCREEN_HEIGHT = 560;
-SDL_Rect tiles[boardSize][boardSize];
+int SCREEN_WIDTH = 700;
+int SCREEN_HEIGHT =400;
+SDL_Rect boardTiles[boardSize][boardSize];
+
+
 
 int ChessBoard[boardSize][boardSize] = {
 {0,0,0,0,0,0,0,0},
@@ -19,6 +21,9 @@ int ChessBoard[boardSize][boardSize] = {
 {0,0,0,0,0,0,0,0}
 
 };
+
+// chess board coords and find chess board coords
+/*
 const string ChessBoardCords[boardSize][boardSize] = {
 {"8a","8b","8c","8d","8e","8f","8g","8h"},
 {"7a","7b","7c","7d","7e","7f","7g","7h"},
@@ -31,7 +36,7 @@ const string ChessBoardCords[boardSize][boardSize] = {
 
 };
 
- SDL_Rect boardTiles[boardSize][boardSize];
+
 
 int findSquareCoords(string coord){
 	for (int i = 0 ; i < boardSize; i++){
@@ -43,6 +48,10 @@ int findSquareCoords(string coord){
 		}
 	}
 }
+*/
+
+
+
 
 void drawBoard(SDL_Renderer* renderer, float width, float height) {
 	int tileWidthInc;
@@ -96,11 +105,65 @@ void drawBoard(SDL_Renderer* renderer, float width, float height) {
 	}
 	cout << endl << " Tile*8: " << tileSize*8 << " Width: " << width << " Height: " << height;
 
-	SDL_RenderPresent(renderer);
+	
 };
 
+void drawPeice(SDL_Renderer* renderer, string peiceType, int column, int row){
+	SDL_Surface* imageSurface = NULL;
+	
 
-void drawPieces(SDL_Renderer* renderer) {
+	if (peiceType == "bP") {
+		imageSurface = SDL_LoadBMP("assets/bP.bmp");
+	}
+	else if(peiceType == "bQ"){
+		imageSurface = SDL_LoadBMP("assets/bQ.bmp");
+	}
+	else if(peiceType == "bK"){
+		imageSurface = SDL_LoadBMP("assets/bK.bmp");
+	}
+	else if (peiceType == "bN") {
+		imageSurface = SDL_LoadBMP("assets/bN.bmp");
+	}
+	else if (peiceType == "bB") {
+		imageSurface = SDL_LoadBMP("assets/bB.bmp");
+	}
+	else if (peiceType == "bR") {
+		imageSurface = SDL_LoadBMP("assets/bR.bmp");
+	}
+	else if (peiceType == "P") {
+		imageSurface = SDL_LoadBMP("assets/P.bmp");
+	}
+	else if (peiceType == "Q") {
+		imageSurface = SDL_LoadBMP("assets/Q.bmp");
+	}
+	else if (peiceType == "K") {
+		imageSurface = SDL_LoadBMP("assets/K.bmp");
+	}
+	else if (peiceType == "N") {
+		imageSurface = SDL_LoadBMP("assets/N.bmp");
+	}
+	else if (peiceType == "B") {
+		imageSurface = SDL_LoadBMP("assets/B.bmp");
+	}
+	else if (peiceType == "R") {
+		imageSurface = SDL_LoadBMP("assets/R.bmp");
+	}
+
+
+	if (NULL == imageSurface) {
+		cout << "SDL error bozo" << SDL_GetError();
+	}
+	SDL_Rect imageRectLocation = boardTiles[column][row];
+	
+	//SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "0");
+	SDL_Texture* imageTexture = SDL_CreateTextureFromSurface(renderer, imageSurface);
+	SDL_FreeSurface(imageSurface);
+	//SDL_SetTextureScaleMode(temTexture, SDL_ScaleModeBest);
+
+	SDL_RenderCopy(renderer, imageTexture, NULL, &imageRectLocation);
+	SDL_DestroyTexture(imageTexture);
+
+	
 }
 
 int main(int argc, char* argv[])
@@ -122,28 +185,48 @@ int main(int argc, char* argv[])
 	SDL_Window* window = SDL_CreateWindow("Chess Express", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 	SDL_Event event;
-
 	//NEW
-	SDL_Surface* imageSurface = NULL;
-	SDL_Surface* windowSurface = NULL;
-	windowSurface = SDL_GetWindowSurface(window);
-	imageSurface = SDL_LoadBMP("B.bmp");
-
-	if (NULL == imageSurface) {
-		cout << "SDLLLLL ERRROOORRR AMICOOOO" << SDL_GetError();
-	}
-
-
+	
+	
 	drawBoard(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
-	SDL_Rect image_rect = boardTiles[5][5];
-	SDL_Texture* temTexture = SDL_CreateTextureFromSurface(renderer, imageSurface);
-	SDL_RenderCopy(renderer, temTexture, nullptr, &image_rect);
-	SDL_RenderPresent(renderer);
+	drawPeice(renderer, "bR", 0, 0);
+	drawPeice(renderer, "bN", 0, 1);
+	drawPeice(renderer, "bB", 0, 2);
+	drawPeice(renderer, "bQ", 0, 3);
+	drawPeice(renderer, "bK", 0, 4);
+	drawPeice(renderer, "bB", 0, 5);
+	drawPeice(renderer, "bN", 0, 6);
+	drawPeice(renderer, "bR", 0, 7);
+	drawPeice(renderer, "bP", 1, 0);
+	drawPeice(renderer, "bP", 1, 1);
+	drawPeice(renderer, "bP", 1, 2);
+	drawPeice(renderer, "bP", 1, 3);
+	drawPeice(renderer, "bP", 1, 4);
+	drawPeice(renderer, "bP", 1, 5);
+	drawPeice(renderer, "bP", 1, 6);
+	drawPeice(renderer, "bP", 1, 7);
 
+	drawPeice(renderer, "R", 7, 0);
+	drawPeice(renderer, "N", 7, 1);
+	drawPeice(renderer, "B", 7, 2);
+	drawPeice(renderer, "Q", 7, 3);
+	drawPeice(renderer, "K", 7, 4);
+	drawPeice(renderer, "B", 7, 5);
+	drawPeice(renderer, "N", 7, 6);
+	drawPeice(renderer, "R", 7, 7);
+	drawPeice(renderer, "P", 6, 0);
+	drawPeice(renderer, "P", 6, 1);
+	drawPeice(renderer, "P", 6, 2);
+	drawPeice(renderer, "P", 6, 3);
+	drawPeice(renderer, "P", 6, 4);
+	drawPeice(renderer, "P", 6, 5);
+	drawPeice(renderer, "P", 6, 6);
+	drawPeice(renderer, "P", 6, 7);
+	SDL_RenderPresent(renderer);
 	//SDL_UpdateWindowSurface(window);
 
 	// HERES THE GAME LOOP WOOO
-	bool gameRunning = true;
+	bool gameRunning = true; 
 
 	while (gameRunning) {
 		while (SDL_PollEvent(&event)) {
@@ -156,15 +239,54 @@ int main(int argc, char* argv[])
 			if (event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) {
 
 				drawBoard(renderer, event.window.data1, event.window.data2);
+				
+				//Chess thing lol /*
+				drawPeice(renderer, "bR", 0, 0);
+				drawPeice(renderer, "bN", 0, 1);
+				drawPeice(renderer, "bB", 0, 2);
+				drawPeice(renderer, "bQ", 0, 3);
+				drawPeice(renderer, "bK", 0, 4);
+				drawPeice(renderer, "bB", 0, 5);
+				drawPeice(renderer, "bN", 0, 6);
+				drawPeice(renderer, "bR", 0, 7);
+				drawPeice(renderer, "bP", 1, 0);
+				drawPeice(renderer, "bP", 1, 1);
+				drawPeice(renderer, "bP", 1, 2);
+				drawPeice(renderer, "bP", 1, 3);
+				drawPeice(renderer, "bP", 1, 4);
+				drawPeice(renderer, "bP", 1, 5);
+				drawPeice(renderer, "bP", 1, 6);
+				drawPeice(renderer, "bP", 1, 7);
 
+				drawPeice(renderer, "R", 7, 0);
+				drawPeice(renderer, "N", 7, 1);
+				drawPeice(renderer, "B", 7, 2);
+				drawPeice(renderer, "Q", 7, 3);
+				drawPeice(renderer, "K", 7, 4);
+				drawPeice(renderer, "B", 7, 5);
+				drawPeice(renderer, "N", 7, 6);
+				drawPeice(renderer, "R", 7, 7);
+				drawPeice(renderer, "P", 6, 0);
+				drawPeice(renderer, "P", 6, 1);
+				drawPeice(renderer, "P", 6, 2);
+				drawPeice(renderer, "P", 6, 3);
+				drawPeice(renderer, "P", 6, 4);
+				drawPeice(renderer, "P", 6, 5);
+				drawPeice(renderer, "P", 6, 6);
+				drawPeice(renderer, "P", 6, 7);
+				//uh yeah /*
+
+				SDL_RenderPresent(renderer);
+
+				//make a function that displays the current board shape. make a fen notation val called "current"
 			}
 
 			
 		}
 
 	}
-	SDL_FreeSurface(windowSurface);
-	SDL_FreeSurface(imageSurface);
+
+	
 	SDL_DestroyWindow(window);
 
 	SDL_Quit();
